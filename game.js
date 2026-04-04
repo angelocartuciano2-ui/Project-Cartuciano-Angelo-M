5400774bc1b6241bd18e59652cd8f7218224f260
angelo <angelocartuciano2@gmail.com>
2026-04-04 05:15:11 +0300

    js 38 onwards

--- game.js
+++ game.js
@@ -35,4 +35,18 @@ function playGame(playerChoice) {
     // Decide winner
     let result = "";
 
-    
+ 
+    if (playerChoice === computerChoice) {
+        result = "It's a Tie!";
+    } 
+    else if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
+        result = "You Win!";
+    } 
+     else {
+        result = "Computer Wins!";
+    }
+}
